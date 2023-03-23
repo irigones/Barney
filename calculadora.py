12b7d43a7a00c201c3da4e09359362e1087b643d
irigones <127993059+irigones@users.noreply.github.com>
2023-03-22 21:31:51 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,19 @@
+
+a=float(input())
+b=float(input())
+c=input()
+
+
+
+if c=="*":                      # if é para fazer uma condição, se for algum simbolo especifico, vai acontecer alguma coisa em especifico.
+    print(a*b)
+
+elif c=="+":                    # elif = else + if.
+    print(a+b)
+
+elif c=="/":                    # para fazer a condição funcionar, sempre botar ":" no final do código
+    print(a/b)
+
+elif c=="-":                    # quando usar uma condição, como o elif, na linha de baixo botar a condição com um tab antes.
+    print(a-b)
+    
