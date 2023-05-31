2ddcca6388709f28d94a939eb7de40d0607296bb
irigones <127993059+irigones@users.noreply.github.com>
2023-05-31 17:45:16 -0300

    Add files via upload

---
+++ atividade_1-arquivo_2.py
@@ -0,0 +1,15 @@
+lista = [2, 3, 7, 12, 2]
+
+print("lista de números")
+for numero in lista:
+    print(numero)
+
+# Multiplicar cada número por 2
+lista_multiplicada = []
+for numero in lista:
+    lista_multiplicada.append(numero * 2)
+
+# Imprimir os números multiplicados por 2
+print("lista de números x 2")
+for numero in lista_multiplicada:
+    print(numero)
