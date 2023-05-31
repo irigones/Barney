dc5e4951a15c18564c143cf8ed3aec23ed17b3ca
irigones <127993059+irigones@users.noreply.github.com>
2023-05-31 17:46:00 -0300

    Add files via upload

---
+++ atividade_1-arquivo_3.py
@@ -0,0 +1,7 @@
+nomes = ["Alexandre", "Alice", "Andre", "Arthur", "Arthur", "Artur", "Augusto", "Bernardo", "Bernardo", "Bruno", "Davi", "Diego", "Eduardo", "Fabricio", "Felipe", "Fernando", "Francisco", "Francisco", "Gabriel", "Gabriel", "Giovanna", "Giovanni", "Guilherme", "Guilherme", "Hector", "Henrique", "Inacio", "Joao", "Joao", "Joaquim", "Julia", "Lauren", "Leonardo", "Leonardo", "Lucas", "Marina", "Matheus", "Matheus", "Paula", "Pedro", "Pedro", "Pedro", "Pedro", "Rafael", "Regis", "Sofia", "Stella", "Thiago", "Valentina", "Vicente", "Lucas"]
+
+nomes_organizados = sorted(nomes)
+
+print("lista de nomes em ordem alfabética")
+for nome in nomes_organizados:
+    print(nome)
